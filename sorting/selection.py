c79f19ac6de62cafda31fa835589423ca51d3ed4
yiohans <yiohans@users.noreply.github.com>
2025-02-22 19:52:23 -0300

    chore[sorting]: Update comments in selection sort algorithm for clarity

--- sorting/selection.py
+++ sorting/selection.py
@@ -2,7 +2,8 @@
 def selection_sort(arr):
     # Get the length of the array
     n = len(arr)
-    # Traverse through all array elements
+    # Increase the start position when the minimum element is found and swaped
+    # with the element in the current start position
     print(f"Initial state: {arr}")
     for start_element in range(n):
         # Iterate through the array starting from the current start element
@@ -12,6 +13,7 @@ def selection_sort(arr):
             # update the minimum element index
             if arr[current_element] < arr[min_element_index]:
                 min_element_index = current_element
+        # Swap the current start position and minimum elements
         arr[start_element], arr[min_element_index] = arr[min_element_index], arr[start_element]
         print(f"Current stage: {arr}")
         
